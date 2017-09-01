03fa5b1065015037554ba5d15f96fd296c3d5f84
Dimitar Nestorov <mitkones@gmail.com>
2017-09-01 21:38:11 +0300

    Removing localStorage in AuthService It was redundant (Firebase is using it)

--- project/src/app/auth/auth.service.ts
+++ project/src/app/auth/auth.service.ts
@@ -8,60 +8,47 @@ import { Observable } from 'rxjs/Observable';
 export class AuthService {
 
   user: Observable<firebase.User>;
+  token: String;
 
   constructor(private router: Router, private af: AngularFireAuth) {
     this.user = af.authState;
+
+    this.user.subscribe((user) =>
+      user ? user.getToken()
+      .then((token: string) => this.token = token) : 0
+    );
   }
 
   signupUser(email: string, password: string) {
     this.af.auth.createUserWithEmailAndPassword(email, password)
-    .then((success) => {
-      const user: any = this.af.auth.currentUser;
-      user.sendEmailVerification().then(
-        () => {
-          alert('Please verify your email.');
-      });
+    .then(() => {
+      this.router.navigate(['/']);
+      this.af.auth.currentUser.sendEmailVerification()
+      .then(() => alert('Please verify your email.'));
     });
   }
 
   signinUser(email: string, password: string) {
     this.af.auth.signInWithEmailAndPassword(email, password)
-    .then(response => {
-      this.router.navigate(['/']);
-      this.af.auth.currentUser.getToken()
-      .then((token: string) => {
-        localStorage.setItem('token', token);
-        localStorage.setItem('uid', this.af.auth.currentUser.uid);
-        localStorage.setItem('email', this.af.auth.currentUser.email);
-      });
-    });
+    .then(response => this.router.navigate(['/']));
   }
 
   signinGmail() {
     this.af.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-    .then(response => {
-      this.router.navigate(['/']);
-      this.af.auth.currentUser.getToken()
-      .then((token: string) => {
-        localStorage.setItem('token', token);
-        localStorage.setItem('uid', this.af.auth.currentUser.uid);
-        localStorage.setItem('email', this.af.auth.currentUser.email);
-      });
-    });
+    .then(response => this.router.navigate(['/']));
   }
 
   logout() {
     this.af.auth.signOut();
-    localStorage.clear();
   }
 
   getEmail() {
-    return localStorage.getItem('email');
+    return this.af.auth.currentUser.email;
   }
   getToken() {
-    return localStorage.getItem('token');
+    return this.token;
   }
-  isAuthenticated() {
-    return localStorage.getItem('token') != null;
+  isAuthenticated(): boolean {
+    return this.af.auth.currentUser ? true : false;
   }
 }
